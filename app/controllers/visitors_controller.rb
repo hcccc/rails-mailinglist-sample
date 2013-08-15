2fa8bdbd70ec8a24bb570f217cb574857a08887f
chuan-he <chuan.he@gree.net>
2013-08-15 20:07:43 +0900

    Added simple bootstrap tempalte

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,8 +1,10 @@
 class VisitorsController < ApplicationController
 
 	def new
+		Rails.logger.debug 'DEBUG: entering new method'
 		@owner = Owner.new
-		render 'visitors/new'
+		flash[:notice] = 'Welcome!'
+		flash[:alert] = 'My Birthday is soon'
 	end
 
 end
