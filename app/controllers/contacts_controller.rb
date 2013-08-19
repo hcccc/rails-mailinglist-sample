3f13c5446ab2092737b3f1ef2847bb4bf7ae1b14
chuan-he <chuan.he@gree.net>
2013-08-19 13:40:36 +0900

    added rails way of contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -15,11 +15,6 @@ class ContactsController < ApplicationController
 
     end
 
-    def process_form
-        Rails.logger.debug 'DEBUG: params are' + params.inspect
-        flash[:notice] = "Received request from #{params[:contact][:name]}"
-        redirect_to root_path
-    end
 
     private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 LearnRails::Application.routes.draw do
 
   resources :contacts, only: [:new, :create]
-  post 'contact' => 'contacts#process_form'
+  #post 'contact' => 'contacts#process_form'
   root :to => 'visitors#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
